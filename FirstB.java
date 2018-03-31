826a875d6e772563e145b2fbcc5e57b71fa677b7
Nadya <33684022+prok27na@users.noreply.github.com>
2018-03-31 13:36:25 +0500

    Добавлено во 2 форму "Копирайтер" - scrollbar и кнопка расчета. Добавлена 3 форма "Заказчик"

---
+++ FirstB.java
@@ -0,0 +1,21 @@
+// Создаем поле для ввода текста 
+final JTextArea area = new JTextArea("Введите сюда свой текст...",20, 50); 
+//Создаем полосу прокрутки 
+JScrollPane scrollBar = new JScrollPane(area); 
+//Добавляем полосу прокрутки на текстовое поле 
+frame.getContentPane().add(scrollBar, FlowLayout()); 
+
+
+/* 
+* Создаем "слушателя" для кнопки расчета символов. Если поле area не пустое, то 
+* подсчитывается количество символов и слов, иначе выводит ошибку: 
+*/ 
+countButton.addActionListener(new ActionListener() { 
+@Override 
+public void actionPerformed(ActionEvent e) { 
+if (area.getText().trim().length() != 0) 
+result.setText("Количество символов: " + area.getText().length() + ". Количество слов: " + area.getText().trim().split("\\s+").length); 
+else 
+result.setText("Сперва введите текст!"); 
+} 
+}); 
